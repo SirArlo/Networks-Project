4f8953cc45e807938e451666776006e2076b5552
EmulRoss <emul.ross@gmail.com>
2018-03-14 17:20:53 +0200

    Updated GUI

--- InterfaceClient.py
+++ InterfaceClient.py
@@ -2,6 +2,7 @@ import Tkinter
 
 from Tkinter import *
 import ttk
+import FTP_Client
 
 class initialize_Window(Tkinter.Frame):
     def __init__(self, parent):
@@ -18,42 +19,89 @@ class initialize_Window(Tkinter.Frame):
         self.parent.title("File Transfer Protocol")
         self.parent.grid_columnconfigure(0, weight = 1)
         self.parent.grid_rowconfigure(0, weight = 1)
-        self.parent.config(background = "white")
+        self.parent.config(background = "#222222")
 
     def Login_interface(self):
-        self.parent.title("File Transfer Protocol")
-        self.parent.grid_columnconfigure(0, weight = 1)
-        self.parent.grid_rowconfigure(0, weight = 1)
-        self.parent.config(background = "white")
 
         self.Username_label = Tkinter.Label(self.parent, text = "Username: ")
+        self.Username_label.place(x = 250, y = 250, width = 100, height =50)
         self.Username_entry = Tkinter.Entry(self.parent)
-        self.Username_label.grid(row = 0, column = 0, sticky = Tkinter.W)
-        self.Username_entry.grid(row = 0, column = 1)
+        self.Username_entry.place(x = 350, y = 250, width = 100, height =50)
         
         self.Password_label = Tkinter.Label(self.parent, text = "Password: ")
+        self.Password_label.place(x = 250, y = 300, width = 100, height =50)
         self.Password_entry = Tkinter.Entry(self.parent)
-        self.Password_label.grid(row = 2, column = 0, sticky = Tkinter.W)
-        self.Password_entry.grid(row = 2, column = 1)
+        self.Password_entry.place(x = 350, y = 300, width = 100, height =50)
+        
+        self.Login_Button = Tkinter.Button(self.parent, text = "Login", command = self.Check_login)
+        self.Login_Button.place(x = 250, y = 350, width = 200, height =50)
+
+        self.Exit_Button = Tkinter.Button(self.parent, text = "Exit", command = self.parent.quit)
+        self.Exit_Button.place(x = 250, y = 400, width = 200, height =50)
+
+    def Check_login(self):
+        #if FTP_Client.isValidUser
+        #    Clear_login()
+        #else:
+        #    return
+        self.Clear_login()
+
+    def Clear_login(self):
+        self.Username_label.destroy()
+        self.Username_entry.destroy()
+        self.Password_label.destroy()
+        self.Password_entry.destroy()
+        self.Login_Button.destroy()
+        self.Exit_Button.destroy()
+        self.FTP_interface()
 
     def FTP_interface(self):
-        self.account_label = Tkinter.Label(self.parent, text = "Account: ")
-        self.account_entry = Tkinter.Entry(self.parent)
-        self.account_label.grid(row = 0, column = 0, sticky = Tkinter.W)
-        self.account_entry.grid(row = 0, column = 1)
+        
+        Account = "NewUser"
+        NumberOfFiles = "0"
+        TotalTransfer = "0"
+        CurrentTransfer = "0"
+        TotalElapsed = "0"
+        CurrentElapsed = "0"
+#account - current account logged in with 
+        self.Account_Label = Tkinter.Label(self.parent, text = "Account: " + str(Account))
+        self.Account_Label.place(x = 0, y = 0, width = 200, height =25)
+#serverport - edit
+        self.ServerPort_Label = Tkinter.Label(self.parent, text = "Enter alternative server port:")
+        self.ServerPort_Label.place(x = 200, y = 0, width = 200, height =25)
+        self.ServerPort_Entry = Tkinter.Entry(self.parent)
+        self.ServerPort_Entry.place(x = 400, y = 0, width = 200, height =25)
+#Hostaddress - edit
+        self.HostAddress_Label = Tkinter.Label(self.parent, text = "Enter alternative host address:")
+        self.HostAddress_Label.place(x = 200, y = 25, width = 200, height =25)
+        self.HostAddress_Entry = Tkinter.Entry(self.parent)
+        self.HostAddress_Entry.place(x = 400, y = 25, width = 200, height =25)
+#number of files - label
+        self.NumberOf_Files_Label = Tkinter.Label(self.parent, text = "Number of files in directory: " + str(NumberOfFiles))
+        self.NumberOf_Files_Label.place(x = 0, y = 25, width = 200, height =25)
+#logout - button and show other screen
+        self.Logout_Button = Tkinter.Button(self.parent, text = "Logout", command = self.parent.quit)
+        self.Logout_Button.place(x = 600, y = 25, width = 100, height =25)
+#Exit - close
+        self.Exit_Button = Tkinter.Button(self.parent, text = "Exit", command = self.parent.quit)
+        self.Exit_Button.place(x = 600, y = 0, width = 100, height =25)
 
-        self.number_Files_label = Tkinter.Label(self.parent, text = "Number of files: ")
-        self.number_Files_entry = Tkinter.Entry(self.parent)
-        self.number_Files_label.grid(row = 1, column = 0, sticky = Tkinter.W)
-        self.number_Files_entry.grid(row = 1, column = 1)
+#Amount of data transferred - for current transfer and total
+        self.DataTransferTotal_Label = Tkinter.Label(self.parent, text = "Total data transferred (Bytes): " + str(TotalTransfer))
+        self.DataTransferTotal_Label.place(x = 0, y = 75, width = 350, height =25)
+        self.DataTransferCurrent_Label = Tkinter.Label(self.parent, text = "Data transferred in this session (Bytes): " + str(CurrentTransfer))
+        self.DataTransferCurrent_Label.place(x = 0, y = 100, width = 350, height =25)
+#time elapsed - for current transfer
+        self.TotalElapsed_Label = Tkinter.Label(self.parent, text = "Total elapsed transfer time (s): " + str(TotalElapsed))
+        self.TotalElapsed_Label.place(x = 350, y = 75, width = 350, height =25)
+        self.CurrentElapsed_Label = Tkinter.Label(self.parent, text = "Elapsed transfer time in this session (s): " + str(CurrentElapsed))
+        self.CurrentElapsed_Label.place(x = 350, y = 100, width = 350, height =25)
 
-        self.exit_button = Tkinter.Button(self.parent, text = "Exit", command = self.parent.quit)
-        self.exit_button.grid(row = 0, column = 4)
-
+#transfer buttons
         self.Server_To_Client_Button = Tkinter.Button(self.parent, text = ">>", command = self.data_Update)
-        self.Server_To_Client_Button.grid(row = 6, column = 2, sticky = Tkinter.W, padx = 50)
+        self.Server_To_Client_Button.place(x = 250, y = 150, width = 100, height =25)
         self.Client_To_Server_Button = Tkinter.Button(self.parent, text = "<<", command = self.data_Update)
-        self.Client_To_Server_Button.grid(row = 7, column = 2, sticky = Tkinter.W, padx = 50)
+        self.Client_To_Server_Button.place(x = 350, y = 150, width = 100, height =25)
 
 
             #def selectItem(a):
@@ -76,31 +124,31 @@ class initialize_Window(Tkinter.Frame):
 
 
         self.server_label = Tkinter.Label(self.parent, text = "Server")
-        self.server_label.grid(row = 2, column = 1, sticky = Tkinter.W)
+        self.server_label.place(x = 0, y = 150, width = 100, height =25)
         self.ServerTree = ttk.Treeview(self.parent, columns=('File Name', 'File Type'))
         self.ServerTree.heading('#0', text = 'File Index')
         self.ServerTree.heading('#1', text = 'File Name')
         self.ServerTree.heading('#2', text = 'File Type')
-        #self.tree.column('#1', stretch = Tkinter.YES)
-        #self.tree.column('#2', stretch = Tkinter.YES)
-        #self.tree.column('#0', stretch = Tkinter.YES)
-        self.ServerTree.grid(row = 3, rowspan = 14, column = 1)
+        self.ServerTree.column('#0', width = 125)
+        self.ServerTree.column('#1', width = 125)
+        self.ServerTree.column('#2', width = 100)
+        self.ServerTree.place(x = 0, y = 200, width = 350, height =500)
         self.ServerTreeview = self.ServerTree
 
         self.client_label = Tkinter.Label(self.parent, text = "Client")
-        self.client_label.grid(row = 2, column = 3, sticky = Tkinter.W)
+        self.client_label.place(x = 600, y = 150, width = 100, height =25)
         self.ClientTree = ttk.Treeview(self.parent, columns=('File Name', 'File Type'))
         self.ClientTree.heading('#0', text = 'File Index')
         self.ClientTree.heading('#1', text = 'File Name')
         self.ClientTree.heading('#2', text = 'File Type')
-        #self.tree.column('#1', stretch = Tkinter.YES)
-        #self.tree.column('#2', stretch = Tkinter.YES)
-        #self.tree.column('#0', stretch = Tkinter.YES)
-        self.ClientTree.grid(row = 3, rowspan = 14, column = 3)
+        self.ClientTree.column('#0', width = 125)
+        self.ClientTree.column('#1', width = 125)
+        self.ClientTree.column('#2', width = 100)
+        self.ClientTree.place(x = 350, y = 200, width = 350, height =500)
         self.ClientTreeview = self.ClientTree
 
         self.Progress = ttk.Progressbar(self.parent, orient=HORIZONTAL, length=200, mode='determinate')
-        self.Progress.grid(row = 20, columnspan = 5, sticky = 'nsew')
+        self.Progress.place(x = 0, y = 175, width = 700, height =25)
         #self.Progress = pack(side = "bottom")
         self.Progress.start()
 
@@ -112,9 +160,12 @@ class initialize_Window(Tkinter.Frame):
         self.i = self.i + 1
 
 
+
 #Label Server, Client
 def main():
     root = Tkinter.Tk()
+    root.resizable(width=False, height=False)
+    root.geometry('{}x{}'.format(700, 700))
     UserInterface = initialize_Window(root)
     root.mainloop()
 
